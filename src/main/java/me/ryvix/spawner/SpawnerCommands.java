6cc4801fd8677ce8947a9cf6113bdfb7c8889a6d
ty2u <rrhode@gmail.com>
2015-02-25 04:39:30 -0500

    Remove unused import

--- src/main/java/me/ryvix/spawner/SpawnerCommands.java
+++ src/main/java/me/ryvix/spawner/SpawnerCommands.java
@@ -30,7 +30,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 public class SpawnerCommands implements CommandExecutor {
